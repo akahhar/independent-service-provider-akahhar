12e737fed05a335e65d9766a5a5d01cdffd49ea0
sm hasanur <sm.hasanur121@gmail.com>
2022-04-18 23:40:45 +0600

    latest update 2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Wedding-moments
 
-[Website Link](https://nayeem-assignment-10.netlify.app/)
+[Website Link](https://wedding-moments-e90ae.web.app/)
 
 - using React Router Dom V-6
 - using bootstrap 5
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,14 +21,14 @@ const Header = () => {
       <header>
         <div className="container">
           <div className="row">
-            <div className="col-xl-3 col-lg-3 col-md-3 d-flex align-items-center">
+            <div className="col-xl-3 col-lg-3 col-md-3 d-flex align-items-center justify-content-center">
               <div className="logo">
                 <Link to="/">
                   <span>Wedding Moments</span>
                 </Link>
               </div>
             </div>
-            <div className="col-xl-7 col-lg-7 col-md-7 d-flex align-items-center justify-content-end">
+            <div className="col-xl-7 col-lg-7 col-md-7 d-flex align-items-center justify-content-end justify-content-center">
               <div className="menu-bar">
                 <nav>
                   <Link to="/home">Home</Link>
@@ -38,7 +38,7 @@ const Header = () => {
                 </nav>
               </div>
             </div>
-            <div className="col-xl-2 col-lg-2 col-md-2 d-flex align-items-center justify-content-end">
+            <div className="col-xl-2 col-lg-2 col-md-2 d-flex align-items-center justify-content-end justify-content-center">
               {user?.uid ? (
                 <button className="btn" onClick={singOutHandler}>
                   Sign Out
